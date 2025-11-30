c29727f05b8e3f952baf9aa2d2b622215b8bab85
PixelRican <rimercado02@gmail.com>
2025-11-29 20:05:50 -0500

    Remove AnimationPlayer
    I had to remove the AnimationPlayer due to an issue. This can be readded later.

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,19 +1,21 @@
 extends Control
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action('ui_cancel'):
+	if event.is_action_pressed('ui_cancel'):
 		if get_tree().paused:
 			_on_resume_pressed()
 		else:
 			_on_pause_pressed()
 
 func _on_pause_pressed() -> void:
+	#$AnimationPlayer.play('blur')
 	get_tree().paused = true
-	$AnimationPlayer.play('blur')
+	visible = true
 
 func _on_resume_pressed() -> void:
+	#$AnimationPlayer.play_backwards('blur')
 	get_tree().paused = false
-	$AnimationPlayer.play_backwards('blur')
+	visible = false
 
 func _on_quit_to_menu_pressed() -> void:
 	get_tree().paused = false
