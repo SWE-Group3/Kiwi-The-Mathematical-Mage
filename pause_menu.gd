2ff5321ede30497e721ace7e5ac9192280fe53fa
PixelRican <rimercado02@gmail.com>
2025-11-30 08:44:01 -0500

    Clean up button events

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,18 +3,21 @@ extends Control
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed('ui_cancel'):
 		if get_tree().paused:
-			_on_resume_pressed()
+			_on_resume_button_pressed()
 		else:
-			_on_pause_pressed()
+			_on_pause_button_pressed()
 
-func _on_pause_pressed() -> void:
+func _on_pause_button_pressed() -> void:
 	get_tree().paused = true
 	show()
 
-func _on_resume_pressed() -> void:
+func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
 	hide()
 
-func _on_exit_pressed() -> void:
+func _on_options_button_pressed() -> void:
+	print('Options')
+
+func _on_exit_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file('res://main_menu.tscn')
