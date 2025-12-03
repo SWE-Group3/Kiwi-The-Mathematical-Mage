1c8658df4408e461dfdd2d574aa3891ccaa45d0a
PixelRican <rimercado02@gmail.com>
2025-12-03 17:50:30 -0500

    Got predator spawning to work

--- game.gd
+++ game.gd
@@ -23,21 +23,4 @@ func _on_spell_4_pressed() -> void:
 
 
 func _on_mob_timer_timeout():
-	#var follower := $Enemy1spawn
-	#follower.set_script(load("res://follower_script.gd"))
-	
-	var mob = mob_scene.instantiate()
-	var mob_spawn_location = $EnemyPath/Enemy1spawn
-	mob_spawn_location.progress_ratio = 0
-	
-	mob.position = mob_spawn_location.position
-	add_child(mob)
-
-	# Add follower to your path
-	#$EnemyPath.add_child(follower)
-
-	# Add mob as a child of the follower
-	#follower.add_child(mob)
-
-	# Ensure mob starts at center of follower
-	
+	$EnemyPath.add_child(mob_scene.instantiate())
