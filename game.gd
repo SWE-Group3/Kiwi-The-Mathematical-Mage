edfaa0bf02978797245188e970e2bdebc05529b6
zammera <zammerallen@gmail.com>
2025-12-03 12:59:22 -0500

    added temp enemies that can move along a given path

--- game.gd
+++ game.gd
@@ -1,5 +1,11 @@
 extends Node
 
+@export var mob_scene: PackedScene
+var score
+
+func _ready() -> void:
+	$MobTimer.start()
+
 func _on_pause_pressed() -> void:
 	print("Pause Pressed")
 
@@ -14,3 +20,20 @@ func _on_spell_3_pressed() -> void:
 
 func _on_spell_4_pressed() -> void:
 	print("Casting Spell 4")
+
+
+func _on_mob_timer_timeout():
+	var follower := PathFollow2D.new()
+	follower.set_script(load("res://follower_script.gd"))
+	
+	var mob := mob_scene.instantiate()
+
+	# Add follower to your path
+	$EnemyPath.add_child(follower)
+
+	# Add mob as a child of the follower
+	follower.add_child(mob)
+
+	# Ensure mob starts at center of follower
+	mob.position = Vector2.ZERO
+	mob.rotation = 0
