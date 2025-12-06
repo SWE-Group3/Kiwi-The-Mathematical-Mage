5bb4e371bf3b56d17e39429c8e6a91604c04ec05
zammera <zammerallen@gmail.com>
2025-12-05 19:05:16 -0500

    made the interface more readable

--- ui/game/user_interface.gd
+++ ui/game/user_interface.gd
@@ -5,6 +5,8 @@ extends CanvasLayer
 func _ready() -> void:
 	Global.wave_started.connect(_on_start_wave)
 	Global.wave_completed.connect(_on_wave_completed)
+	$BottomHUD/ProblemContainer.visible = false
+	$BottomHUD/ColorRect.visible = false
  
 func _on_start_wave(wave: int) -> void:
 	$SpawnTimer.start()
@@ -14,10 +16,18 @@ func _on_start_wave_button_pressed() -> void:
 	print("start wave button pressed")
 	$BottomHUD/StartWaveButton.visible = false
 	$BottomHUD/UpgradesButton.visible = false
+	$BottomHUD/ProblemContainer.visible = true
+	$BottomHUD/ColorRect.visible = true
+
+
 	Global.start_wave()
 	
 	
 func _on_wave_completed(wave: int):
 	$BottomHUD/StartWaveButton.visible = true
 	$BottomHUD/UpgradesButton.visible =true
+	$BottomHUD/ProblemContainer.visible = false
+	$BottomHUD/ColorRect.visible = false
+
+
 	
