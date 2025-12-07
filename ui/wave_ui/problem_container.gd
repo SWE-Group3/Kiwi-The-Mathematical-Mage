7d8ff3b15565bdbdab9f9e6c8f80c675cfcc2445
PixelRican <rimercado02@gmail.com>
2025-12-07 09:48:28 -0500

    Fix enter button

--- ui/wave_ui/problem_container.gd
+++ ui/wave_ui/problem_container.gd
@@ -30,7 +30,6 @@ func _on_submit_button_pressed() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		_on_submit_button_pressed()
-		_focus_answer_box()
 
 func _focus_answer_box() -> void:
 	$AnswerTextField.grab_focus()
