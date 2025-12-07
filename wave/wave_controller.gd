4e4f8cb0dee92dfa3bc12dbbcd1d01c44c6e16cb
PixelRican <rimercado02@gmail.com>
2025-12-06 20:12:44 -0500

    Fix more stuff

--- wave/wave_controller.gd
+++ wave/wave_controller.gd
@@ -26,7 +26,6 @@ func _on_spell_3_pressed() -> void:
 func pick_enemies(points: int) -> Dictionary:
 	var enemies: Array[PackedScene] = []
 	var points_left = points
-	
 	# loops while there are still points
 	while points_left > 0:
 		var pickEnemy = randi_range(1, 3) # pick which enemy to buy
@@ -47,18 +46,16 @@ func pick_enemies(points: int) -> Dictionary:
 					enemy_scene = preload("res://predators/dog/dog_path_follow.tscn") # loads the dog scene in
 			_: # default
 				print("no enemy chosen")
-
 		# Add to the list
 		if enemy_scene != null: # only adds if there is an enemy_scene
 			enemies.append(enemy_scene) # adds the enemy_scene to an array
 			points_left -= cost # subtracts cost of enemy from total points
-
 	# Return both values
 	return {
 		"count": enemies.size(), # number of enemies spawned
 		"list": enemies # list of enmies to spawn
 	}
-	
+
 # Takes in an Array of enemy_scenes and spawns them with a one second delay
 func spawn_enemies(enemies: Array):
 	for enemy in enemies: # gets one enemy from the array
