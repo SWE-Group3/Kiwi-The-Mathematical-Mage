07f5e1c835546980e5613c13438a2fc3f37e95fe
PixelRican <rimercado02@gmail.com>
2025-12-06 21:04:52 -0500

    Add options menu to pause menu

--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -1,16 +1,13 @@
 extends Control
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed('ui_cancel'):
+	if event.is_action_pressed("ui_cancel"):
 		get_tree().paused = not get_tree().paused
 		visible = not visible
 
 func _on_resume_button_pressed() -> void:
 	get_tree().paused = false
 	hide()
-
-func _on_options_button_pressed() -> void:
-	print('Options')
 
 func _on_exit_button_pressed() -> void:
 	get_tree().paused = false
