3e47da51743624e75266f9617bf3ab2a6e28fb58
PixelRican <rimercado02@gmail.com>
2025-12-07 10:47:53 -0500

    Fix spells being cast outside of tilemap

--- ui/wave_ui/tile_map_layer.gd
+++ ui/wave_ui/tile_map_layer.gd
@@ -1,18 +1,15 @@
 extends TileMapLayer
 
-func _input(event):
-	# Only do anything if a spell is selected
-	if SpellManager.selected_spell == null:
-		return
-	# Detect left mouse click
-	if event is InputEventMouseButton \
-	and event.button_index == MOUSE_BUTTON_LEFT \
-	and event.pressed:
-		var mouse_position = get_global_mouse_position()
-		print("Casting spell at: ", mouse_position)
-		# Call the spell manager to do the effect + damage
-		SpellManager.cast_spell_at(mouse_position)
-		# Reset selection (must click button again)
-		SpellManager.selected_spell = null
-		$AudioStreamPlayer.volume_linear = GameController.settings.sound_effects_volume
-		$AudioStreamPlayer.play()
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	var mouse_event := event as InputEventMouseButton
+	# Only do anything if a spell is selected upon left click.
+	if (SpellManager.selected_spell != null and
+		mouse_event and mouse_event.pressed and
+		mouse_event.button_index == MOUSE_BUTTON_LEFT):
+			prints("Casting spell at:", mouse_event.position)
+			# Call the spell manager to do the effect + damage
+			SpellManager.cast_spell_at(mouse_event.position)
+			# Reset selection (must click button again)
+			SpellManager.selected_spell = null
+			$AudioStreamPlayer.volume_linear = GameController.settings.sound_effects_volume
+			$AudioStreamPlayer.play()
